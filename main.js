74c9b0f7cce8c7466c8bdbe28660478d7c3720f7
elartico <polarbear10837@yahoo.com.tw>
2013-10-20 15:09:37 +0800

    Rewrite HTML5.fix #10

--- main.js
+++ main.js
@@ -1,59 +1,37 @@
-gameModule=(function() {
-  var timeoutVar,
-  counter = 0;
- 
-  var colors = ['#ff0000' , '#0000ff' , 'yellow'];
-   var length = colors.length;
+gameModule=(function)(){
+	var timeoutVar,
+	counter=0;
+	function start(){
+	var canvas=document.getElementById("game");
+var ctx=canvas.getContext("2d");
+var ballX=Math.floor(Math.random()*300);
+var ballY=Math.floor(Math.random()*500);
+var ballR=Math.floor(Math.random()*100);
 
-   function touchEvent(evt){
-    var x = evt.clientX,
-    y = evt.clientY;
-
-console.log("Click :" + x + " , " + y);
-     }  
-
-function start(){
-document.getElementById("main").addEventListener("click" , touchEvent , false);
-startGame(); 
-}
-
-
-function startGame(){
-var canvas = document.getElementById('game');
-var ctx = canvas.getContext('2d');
-var ballX = Math.floor(Math.random() * 600);
-var ballY = Math.floor(Math.random() * 450);
-var ballR = Math.floor(Math.random() * 80);
-
-canvas.width=640;
-canvas.height=480;
-
-
-ctx.fillStyle = [counter % length];
+canvas.width=480;
+canvas.height=320;
+ctx.fillStyle='blue';
 ctx.beginPath();
-ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
+ctx.arc(ballX,ballY,ballR,0,Math.PI*2,true);
 ctx.fill();
-
 if(counter>=10){
-gameOver();
-}else{
-  timeoutVar = setTimeout(start,1000);
-counter = counter+1;
- }
-}
 
+}else{
+	timeoutVar=setTimeout(start,1000);
+}
+counter = counter+1;}}
 function gameOver(){
-console.log("Counter:" + counter);  
+console.log("Counter:"+counter);	
 } 
 return{
-             start:start
-         }
+	start:start
+}
 }) ();
-
 gameModule.start();
 
 
 
 
 
-  
+	}
+}
